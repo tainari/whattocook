b122ed31eab97e76f62a1fda7d4a3789519f2db3
tainari <alexandra.jacunski@gmail.com>
2023-06-23 19:11:50 -0400

    Following along with Mosh. Not convinced of my model structures, yeesh.

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import Recipe, Ingredient, RecipeIngredients, RecipeSteps
 # Register your models here.
+
+admin.site.register(Recipe)
+
+admin.site.register(RecipeIngredients)
+
+admin.site.register(RecipeSteps)
+
+admin.site.register(Ingredient)
--- recipes/models.py
+++ recipes/models.py
@@ -3,6 +3,16 @@ from django.db import models
 # Create your models here.
 class Ingredient(models.Model):
     name = models.CharField(max_length=255)
+    category = models.CharField(max_length=255,default=None)
+    #calories_per_gram = models.FloatField()
+
+
+class RecipeIngredients(models.Model):
+    name = models.CharField(max_length=255)
+    models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+
+class RecipeSteps(models.Model):
+    name = models.CharField(max_length=255)
 
 class Recipe(models.Model):
     name = models.CharField(max_length=255)
@@ -11,13 +21,5 @@ class Recipe(models.Model):
     texture_category = models.CharField(max_length=255)
     fillingness_category = models.CharField(max_length=255)
     cook_time = models.IntegerField()
-    #ingredients = models.ForeignKey(RecipeIngredients, on_delete=models.CASCADE)
-
-
-class RecipeIngredients(models.Model):
-    name = models.CharField(max_length=255)
-    #models.ForeignKey(Ingredient,on_delete=models.CASCADE)
-
-class RecipeSteps(models.Model):
-    name = models.CharField(max_length=255)
-    #models.ForeignKey(Recipe,on_delete=models.CASCADE)
+    ingredients = models.ForeignKey(RecipeIngredients, on_delete=models.CASCADE,default=None)
+    steps = models.ForeignKey(RecipeSteps,on_delete=models.CASCADE,default=None)
